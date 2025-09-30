bb7fcefc839fdab146654373ceb63ef69aedae2b
Alexander R Young <yxk925@hotmail.com>
2025-09-30 12:30:41 +0800

    to run with vpython 7.6.5

--- nodes/display_3D_visualization.py
+++ nodes/display_3D_visualization.py
@@ -53,69 +53,69 @@ rospy.on_shutdown(shutdown_hook)
 
 
 # Main scene
-scene=display(title="9DOF Razor IMU Main Screen")
+scene=canvas(title="9DOF Razor IMU Main Screen")
 #scene.range=(1.3,1.3,1.3)
 scene.range=1.3
-scene.forward = (1,0,-0.25)
+scene.forward = vector(1,0,-0.25)
 # Change reference axis (x,y,z) - z is up
-scene.up=(0,0,1)
+scene.up = vector(0,0,1)
 scene.width=500
 scene.height=500
 
 # Second scene (Roll, Pitch, Yaw)
-scene2 = display(title='9DOF Razor IMU Roll, Pitch, Yaw',x=550, y=0, width=500, height=500,center=(0,0,0), background=(0,0,0))
+scene2 = canvas(title='9DOF Razor IMU Roll, Pitch, Yaw',x=550, y=0, width=500, height=500,center=vector(0,0,0), background=vector(0,0,0))
 #scene2.range=(1,1,1)
 scene2.range=1
 scene2.select()
 #Roll, Pitch, Yaw
 #Default reference, i.e. x runs right, y runs up, z runs backward (out of screen)
-cil_roll = cylinder(pos=(-0.5,0.3,0),axis=(0.2,0,0),radius=0.01,color=color.red)
-cil_roll2 = cylinder(pos=(-0.5,0.3,0),axis=(-0.2,0,0),radius=0.01,color=color.red)
-cil_pitch = arrow(pos=(0.5,0.3,0),axis=(0,0,-0.4),shaftwidth=0.02,color=color.green)
-arrow_course = arrow(pos=(0.0,-0.4,0),color=color.cyan,axis=(0,0.2,0), shaftwidth=0.02, fixedwidth=1)
+cil_roll = cylinder(pos=vector(-0.5,0.3,0),axis=vector(0.2,0,0),radius=0.01,color=color.red)
+cil_roll2 = cylinder(pos=vector(-0.5,0.3,0),axis=vector(-0.2,0,0),radius=0.01,color=color.red)
+cil_pitch = arrow(pos=vector(0.5,0.3,0),axis=vector(0,0,-0.4),shaftwidth=0.02,color=color.green)
+arrow_course = arrow(pos=vector(0.0,-0.4,0),color=color.cyan,axis=vector(0,0.2,0), shaftwidth=0.02, fixedwidth=1)
 
 #Roll,Pitch,Yaw labels
-label(pos=(-0.5,0.6,0),text="Roll (degrees / radians)",box=0,opacity=0)
-label(pos=(0.5,0.6,0),text="Pitch (degrees / radians)",box=0,opacity=0)
-label(pos=(0.0,0.02,0),text="Yaw (degrees / radians)",box=0,opacity=0)
-label(pos=(0.0,-0.16,0),text="N",box=0,opacity=0,color=color.yellow)
-label(pos=(0.0,-0.64,0),text="S",box=0,opacity=0,color=color.yellow)
-label(pos=(-0.24,-0.4,0),text="W",box=0,opacity=0,color=color.yellow)
-label(pos=(0.24,-0.4,0),text="E",box=0,opacity=0,color=color.yellow)
-label(pos=(0.18,-0.22,0),height=7,text="NE",box=0,color=color.yellow)
-label(pos=(-0.18,-0.22,0),height=7,text="NW",box=0,color=color.yellow)
-label(pos=(0.18,-0.58,0),height=7,text="SE",box=0,color=color.yellow)
-label(pos=(-0.18,-0.58,0),height=7,text="SW",box=0,color=color.yellow)
+label(pos=vector(-0.5,0.6,0),text="Roll (degrees / radians)",box=0,opacity=0)
+label(pos=vector(0.5,0.6,0),text="Pitch (degrees / radians)",box=0,opacity=0)
+label(pos=vector(0.0,0.02,0),text="Yaw (degrees / radians)",box=0,opacity=0)
+label(pos=vector(0.0,-0.16,0),text="N",box=0,opacity=0,color=color.yellow)
+label(pos=vector(0.0,-0.64,0),text="S",box=0,opacity=0,color=color.yellow)
+label(pos=vector(-0.24,-0.4,0),text="W",box=0,opacity=0,color=color.yellow)
+label(pos=vector(0.24,-0.4,0),text="E",box=0,opacity=0,color=color.yellow)
+label(pos=vector(0.18,-0.22,0),height=7,text="NE",box=0,color=color.yellow)
+label(pos=vector(-0.18,-0.22,0),height=7,text="NW",box=0,color=color.yellow)
+label(pos=vector(0.18,-0.58,0),height=7,text="SE",box=0,color=color.yellow)
+label(pos=vector(-0.18,-0.58,0),height=7,text="SW",box=0,color=color.yellow)
 
-rollLabel = label(pos=(-0.5,0.52,0),text="-",box=0,opacity=0,height=12)
-pitchLabel = label(pos=(0.5,0.52,0),text="-",box=0,opacity=0,height=12)
-yawLabel = label(pos=(0,-0.06,0),text="-",box=0,opacity=0,height=12)
+rollLabel = label(pos=vector(-0.5,0.52,0),text="-",box=0,opacity=0,height=12)
+pitchLabel = label(pos=vector(0.5,0.52,0),text="-",box=0,opacity=0,height=12)
+yawLabel = label(pos=vector(0,-0.06,0),text="-",box=0,opacity=0,height=12)
 
 #acceleration labels
-label(pos=(0,0.9,0),text="Linear Acceleration x / y / z (m/s^2)",box=0,opacity=0)
-label(pos=(0,-0.8,0),text="Angular Velocity x / y / z (rad/s)",box=0,opacity=0)
-linAccLabel = label(pos=(0,0.82,0),text="-",box=0,opacity=0,height=12)
-angVelLabel = label(pos=(0,-0.88,0),text="-",box=0,opacity=0,height=12)
+label(pos=vector(0,0.9,0),text="Linear Acceleration x / y / z (m/s^2)",box=0,opacity=0)
+label(pos=vector(0,-0.8,0),text="Angular Velocity x / y / z (rad/s)",box=0,opacity=0)
+linAccLabel = label(pos=vector(0,0.82,0),text="-",box=0,opacity=0,height=12)
+angVelLabel = label(pos=vector(0,-0.88,0),text="-",box=0,opacity=0,height=12)
 
 # Main scene objects
 scene.select()
 # Reference axis (x,y,z) - using ROS conventions (REP 103) - z is up, y left (west, 90 deg), x is forward (north, 0 deg)
 # In visual, z runs up, x runs forward, y runs left (see scene.up command earlier) 
 # So everything is good
-arrow(color=color.green,axis=(1,0,0), shaftwidth=0.04, fixedwidth=1)
-arrow(color=color.green,axis=(0,1,0), shaftwidth=0.04 , fixedwidth=1)
-arrow(color=color.green,axis=(0,0,1), shaftwidth=0.04, fixedwidth=1)
+arrow(color=color.green,axis=vector(1,0,0), shaftwidth=0.04, fixedwidth=1)
+arrow(color=color.green,axis=vector(0,1,0), shaftwidth=0.04 , fixedwidth=1)
+arrow(color=color.green,axis=vector(0,0,1), shaftwidth=0.04, fixedwidth=1)
 
 # labels
-label(pos=(0,0,-1.2),text="Press 'a' to align",box=0,opacity=0)
-label(pos=(1,0.1,0),text="X",box=0,opacity=0)
-label(pos=(0,1,-0.1),text="Y",box=0,opacity=0)
-label(pos=(0,-0.1,1),text="Z",box=0,opacity=0)
+label(pos=vector(0,0,-1.2),text="Press 'a' to align",box=0,opacity=0)
+label(pos=vector(1,0.1,0),text="X",box=0,opacity=0)
+label(pos=vector(0,1,-0.1),text="Y",box=0,opacity=0)
+label(pos=vector(0,-0.1,1),text="Z",box=0,opacity=0)
 # IMU object
-platform = box(length=1.0, height=0.05, width=0.65, color=color.red,up=(0,0,1),axis=(-1,0,0))
-p_line = box(length=1.1,height=0.08,width=0.1,color=color.yellow,up=(0,0,1),axis=(-1,0,0))
-plat_arrow = arrow(length=-0.8,color=color.cyan,up=(0,0,1),axis=(-1,0,0), shaftwidth=0.06, fixedwidth=1)
-plat_arrow_up = arrow(length=0.4,color=color.cyan,up=(-1,0,0),axis=(0,0,1), shaftwidth=0.06, fixedwidth=1)
+platform = box(length=1.0, height=0.05, width=0.65, color=color.red,up=vector(0,0,1),axis=vector(-1,0,0))
+p_line = box(length=1.1,height=0.08,width=0.1,color=color.yellow,up=vector(0,0,1),axis=vector(-1,0,0))
+plat_arrow = arrow(length=-0.8,color=color.cyan,up=vector(0,0,1),axis=vector(-1,0,0), shaftwidth=0.06, fixedwidth=1)
+plat_arrow_up = arrow(length=0.4,color=color.cyan,up=vector(-1,0,0),axis=vector(0,0,1), shaftwidth=0.06, fixedwidth=1)
 rospy.init_node("display_3D_visualization_node")
 
 def processIMU_message(imuMsg):
@@ -135,8 +135,8 @@ def processIMU_message(imuMsg):
     #add align offset to yaw
     yaw += yaw_offset
 
-    axis=(-cos(pitch)*cos(yaw),-cos(pitch)*sin(yaw),sin(pitch)) 
-    up=(sin(roll)*sin(yaw)+cos(roll)*sin(pitch)*cos(yaw),-sin(roll)*cos(yaw)+cos(roll)*sin(pitch)*sin(yaw),cos(roll)*cos(pitch))
+    axis=vector(-cos(pitch)*cos(yaw),-cos(pitch)*sin(yaw),sin(pitch)) 
+    up=vector(sin(roll)*sin(yaw)+cos(roll)*sin(pitch)*cos(yaw),-sin(roll)*cos(yaw)+cos(roll)*sin(pitch)*sin(yaw),cos(roll)*cos(pitch))
     platform.axis=axis
     platform.up=up
     platform.length=1.0
@@ -149,11 +149,11 @@ def processIMU_message(imuMsg):
     p_line.axis=axis
     p_line.up=up
     p_line.length=1.1
-    cil_roll.axis=(-0.2*cos(roll),0.2*sin(roll),0)
-    cil_roll2.axis=(0.2*cos(roll),-0.2*sin(roll),0)
-    cil_pitch.axis=(0,-0.4*sin(pitch),-0.4*cos(pitch))
+    cil_roll.axis=vector(-0.2*cos(roll),0.2*sin(roll),0)
+    cil_roll2.axis=vector(0.2*cos(roll),-0.2*sin(roll),0)
+    cil_pitch.axis=vector(0,-0.4*sin(pitch),-0.4*cos(pitch))
     #remove yaw_offset from yaw display
-    arrow_course.axis=(-0.2*sin(yaw-yaw_offset),0.2*cos(yaw-yaw_offset),0)
+    arrow_course.axis=vector(-0.2*sin(yaw-yaw_offset),0.2*cos(yaw-yaw_offset),0)
     
     #display in degrees / radians
     rollLabel.text = str(round(roll*rad2degrees, precision)) + " / " + str(round(roll,precision))
